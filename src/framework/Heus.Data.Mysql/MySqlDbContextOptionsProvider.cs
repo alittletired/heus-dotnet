566a7c30b7332e93141e3165c214ee02e10ff837
zhul03 <zhul0@mingyuanyun.com>
2022-09-26 21:51:44 +0800

    c

--- src/framework/Heus.Data.Mysql/MySqlDbContextOptionsProvider.cs
+++ src/framework/Heus.Data.Mysql/MySqlDbContextOptionsProvider.cs
@@ -1,17 +1,13 @@
 ﻿using Heus.Data.EfCore;
 using Heus.Ddd.Data.Options;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Heus.Data.Mysql
 {
     internal class MySqlDbContextOptionsProvider : IDbContextOptionsProvider
     {
-        public Action<DbContextOptionsBuilder> OptionsBuilder => optionsBuilder=>optionsBuilder.useMy
+        public Action<DbContextOptionsBuilder> OptionsBuilder => optionsBuilder=>optionsBuilder.UseMySql
 
         public DbProvider DbProvider => DbProvider.MySql;
     }
