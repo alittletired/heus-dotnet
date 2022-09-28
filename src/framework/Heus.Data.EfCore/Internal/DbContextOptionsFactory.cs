f1877c216751301db73c74aa84df71a116f7e303
zhul03 <zhul0@mingyuanyun.com>
2022-09-28 18:04:35 +0800

    c

--- src/framework/Heus.Data.EfCore/Internal/DbContextOptionsFactory.cs
+++ src/framework/Heus.Data.EfCore/Internal/DbContextOptionsFactory.cs
@@ -1,6 +1,8 @@
-﻿using Heus.Core.DependencyInjection;
+﻿using Heus.Core;
+using Heus.Core.DependencyInjection;
 using Heus.Ddd.Uow;
 using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
 
 namespace Heus.Data.EfCore.Internal
 {
@@ -13,14 +15,18 @@ namespace Heus.Data.EfCore.Internal
              IUnitOfWorkManager unitOfWorkManager
             , IEnumerable<IDbContextOptionsProvider> dbContextOptionsProviders)
         {
-         
             _unitOfWorkManager= unitOfWorkManager;
             _dbContextOptionsProviders = dbContextOptionsProviders;
         }
 
         public DbContextOptions<TDbContext> Create<TDbContext>() where TDbContext : DbContext
         {
-
+            var unitOfWork = _unitOfWorkManager.Current;
+            if (unitOfWork == null)
+            {
+                throw new BusinessException("A DbContextOptions can only be created inside a unit of work!");
+            }
+            unitOfWork.
             return null!;
         }
 
--- src/framework/Heus.Ddd/Data/Internal/DefaultConnectionStringResolver.cs
+++ src/framework/Heus.Ddd/Data/Internal/DefaultConnectionStringResolver.cs
@@ -1,12 +1,29 @@
 
 using Heus.Core.DependencyInjection;
+using Heus.Ddd.Data.Options;
+using Microsoft.Extensions.Options;
 
 namespace Heus.Data;
 internal class DefaultConnectionStringResolver: IConnectionStringResolver
 {
-     
+    private readonly IOptions<DbConnectionOptions> _options;
+     public DefaultConnectionStringResolver(IOptions<DbConnectionOptions> options)
+    {
+        _options = options;
+    }
     public Task<string> ResolveAsync(string? connectionStringName = null)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(ResolveInternal(connectionStringName));
+    }
+    private string ResolveInternal(string? connectionStringName)
+    {
+        if (connectionStringName == null)
+        {
+            return _options.Value.ConnectionStrings.Default;
+        }
+
+        return _options.Value.ConnectionStrings[connectionStringName];
+
+     
     }
 }
