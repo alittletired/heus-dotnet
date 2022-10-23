6e885b3f7e9da2968baf420e3b8b6a4753869c8d
zhulin <12405614@qq.com>
2022-10-23 23:50:53 +0800

    c

--- src/modules/enroll/enroll-web/src/services/auth.tsx
+++ src/modules/enroll/enroll-web/src/services/auth.tsx
@@ -1,80 +1,26 @@
 import adminApi, { AuthTokenDto } from '@/api/admin'
 import { useCallback } from 'react'
-
-import { atom, AtomEffect, selector, useRecoilState, useRecoilValue } from 'recoil'
-class MyStorage<T> {
-  private _key: string
-  private _defaultValue: T
-  private _value: T
-  public constructor(key: string, defaultValue: T) {
-    this._key = key
-    this._defaultValue = defaultValue
-    const value = localStorage.getItem(this._key)
-    if (value) {
-      this._value = JSON.parse(value)
-    } else {
-      this._value = this._defaultValue
-    }
-  }
-  private _handler: (value: T) => void
-  public onChange = (handler: (value: T) => void) => {
-    this._handler = handler
-  }
-  public set = (newValue: T) => {
-    this._value = newValue
-    localStorage.setItem(this._key, JSON.stringify(newValue))
-  }
-  public triggerChange = (value: T) => {
-    this.set(value)
-    this._handler?.(value)
-  }
-  public get = () => {
-    return this._value
-  }
-  public reset = () => {
-    this._value = this._defaultValue
-    localStorage.removeItem(this._key)
-  }
-}
-function withStorage<T>(key: string, defaultValue: T) {
-  const myStorage = new MyStorage<T>(key, defaultValue)
-  const effect: AtomEffect<T> = ({ setSelf, trigger, onSet }) => {
-    if (trigger === 'get') {
-      setSelf(myStorage.get())
-    }
-    myStorage.onChange((value) => setSelf(value))
-    onSet((newValue, _, isReset) => {
-      isReset ? myStorage.reset() : myStorage.set(newValue)
-    })
-    return () => {
-      myStorage.onChange(null)
-    }
-  }
-  return { ...myStorage, effect }
-}
+import { selector, useRecoilState, useRecoilValue } from 'recoil'
+import { withStorage } from './storage'
 
 const defaultAuth: AuthToken = { isLogin: false }
-const { get, triggerChange, effect } = withStorage('auth', defaultAuth)
-const authState = atom({
-  key: 'auth',
-  effects: [effect],
-})
+const authStorage = withStorage('auth', defaultAuth)
 
 export interface AuthToken extends AuthTokenDto {
   isLogin?: boolean
 }
 
-export const logout = () => triggerChange(defaultAuth)
+export const logout = authStorage.reset
 
 export const login = (token: AuthToken) => {
-  triggerChange({ ...token, isLogin: true })
+  authStorage.triggerChange({ ...token, isLogin: true })
 }
-export const getAuth = get
-export const useAuth = () => useRecoilState(authState)
+export const getAuth = authStorage.get
+export const useAuth = () => useRecoilState(authStorage.state)
 const permissionState = selector({
   key: 'permissionState',
   get: ({ get }) => {
-    const auth = get(authState)
+    const auth = get(authStorage.state)
     const permissionSet = new Set<string>()
     auth.permissions?.forEach((per) => {
       permissionSet.add(per)
---
+++ src/modules/enroll/enroll-web/src/services/storage.tsx
@@ -0,0 +1,60 @@
+import { atom, AtomEffect, selector, useRecoilState, useRecoilValue } from 'recoil'
+class MyStorage<T> {
+  private _key: string
+  private _defaultValue: T
+  private _value: T
+  public constructor(key: string, defaultValue: T) {
+    this._key = key
+    this._defaultValue = defaultValue
+    const value = localStorage?.getItem(this._key)
+    if (value) {
+      this._value = JSON.parse(value)
+    } else {
+      this._value = this._defaultValue
+    }
+  }
+  private _handlers: Array<(value: T) => void> = []
+  public addListener(handler: (value: T) => void) {
+    this._handlers.push(handler)
+  }
+
+  public removeListener(handler: (value: T) => void) {
+    this._handlers = this._handlers.filter((h) => h != handler)
+  }
+  public set = (newValue: T) => {
+    this._value = newValue
+    localStorage.setItem(this._key, JSON.stringify(newValue))
+  }
+  public triggerChange = (value: T) => {
+    this.set(value)
+    this._handlers.forEach((handler) => handler(value))
+  }
+  public get = () => {
+    return this._value
+  }
+  public reset = () => {
+    this._value = this._defaultValue
+    localStorage.removeItem(this._key)
+  }
+}
+
+export function withStorage<T>(key: string, defaultValue: T) {
+  const myStorage = new MyStorage<T>(key, defaultValue)
+  const effect: AtomEffect<T> = ({ setSelf, trigger, onSet }) => {
+    if (trigger === 'get') {
+      setSelf(myStorage.get())
+    }
+    myStorage.addListener(setSelf)
+    onSet((newValue, _, isReset) => {
+      isReset ? myStorage.reset() : myStorage.set(newValue)
+    })
+    return () => {
+      myStorage.removeListener(setSelf)
+    }
+  }
+  const state = atom({
+    key,
+    effects: [effect],
+  })
+  return { ...myStorage, state }
+}
